0b41b1bc3f9ace656ce79ecf7378c02ad6bf80de
Reakain <reakain@users.noreply.github.com>
2019-04-07 16:54:52 -0400

    Initial Tetris buildout

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TetrisGame
+{
+    public class Spawner : MonoBehaviour
+    {
+        // Groups
+        public GameObject[] groups;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Spawn initial Group
+            spawnNext();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void spawnNext()
+        {
+            // Random Index
+            int i = Random.Range(0, groups.Length);
+
+            // Spawn Group at current Position
+            Instantiate(groups[i],
+                        transform.position,
+                        Quaternion.identity);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Tetris
+Quick Unity Tetris game in Unity version 2018.3.0f2
+
+Developing to run entire tetris game from single game object instantiation/deletion
+
+Base level tetris game started following tutorial: https://noobtuts.com/unity/2d-tetris-game
